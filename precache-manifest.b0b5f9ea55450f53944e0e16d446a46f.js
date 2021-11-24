e8514ab1d0578f56e3724c960e659e94af93bbe9
khulqu15 <khusnul.ninno15@gmail.com>
2021-11-24 10:32:44 +0700

    Fix Deployment

---
+++ precache-manifest.b0b5f9ea55450f53944e0e16d446a46f.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f71a24c1ca20fb465f5",
+    "url": "/sengon_kavling.prototype/css/app.f24f3f2a.css"
+  },
+  {
+    "revision": "0727bd517338c36be52c",
+    "url": "/sengon_kavling.prototype/css/chunk-vendors.7f71c7fb.css"
+  },
+  {
+    "revision": "06aef0648e995031ddcf52848daac61b",
+    "url": "/sengon_kavling.prototype/img/1.06aef064.jpg"
+  },
+  {
+    "revision": "3a5f0fe5a52a9c78d9fb7d584a4a66ab",
+    "url": "/sengon_kavling.prototype/img/1.3a5f0fe5.jpg"
+  },
+  {
+    "revision": "5f88149d779e2f781d0874ac859cc6bb",
+    "url": "/sengon_kavling.prototype/img/1.5f88149d.jpg"
+  },
+  {
+    "revision": "8a4170d893b55d44b6b10681890ed8c0",
+    "url": "/sengon_kavling.prototype/img/1.8a4170d8.jpg"
+  },
+  {
+    "revision": "9a7ee5c636b4f32e5a0ea15b3c4b68ea",
+    "url": "/sengon_kavling.prototype/img/1.9a7ee5c6.jpg"
+  },
+  {
+    "revision": "9c92ad86a67ce3a2cda3dcf170313426",
+    "url": "/sengon_kavling.prototype/img/1.9c92ad86.png"
+  },
+  {
+    "revision": "bb2f78a41f3102d59d5c0afdfd352b22",
+    "url": "/sengon_kavling.prototype/img/1.bb2f78a4.png"
+  },
+  {
+    "revision": "ddf7487498a19c4b2af4e710a4664ba1",
+    "url": "/sengon_kavling.prototype/img/1.ddf74874.jpg"
+  },
+  {
+    "revision": "fa5c3446a5496b0283e46f7639d4cf36",
+    "url": "/sengon_kavling.prototype/img/1.fa5c3446.jpg"
+  },
+  {
+    "revision": "2df0a6a3d0c3a6cb57c4783c15ed4992",
+    "url": "/sengon_kavling.prototype/img/2.2df0a6a3.jpg"
+  },
+  {
+    "revision": "4f5e0b547436dc15e9aeaad995dda4dd",
+    "url": "/sengon_kavling.prototype/img/2.4f5e0b54.jpg"
+  },
+  {
+    "revision": "54e44bb10b058cd20f3d6ed0126ff8d5",
+    "url": "/sengon_kavling.prototype/img/2.54e44bb1.jpg"
+  },
+  {
+    "revision": "9894ce7a5bce6451d4706221ef6e1351",
+    "url": "/sengon_kavling.prototype/img/2.9894ce7a.jpg"
+  },
+  {
+    "revision": "9c5468e6c85a9d6fd4f9741fe937d8f5",
+    "url": "/sengon_kavling.prototype/img/2.9c5468e6.jpg"
+  },
+  {
+    "revision": "c677f40164ceb23bf44631fad9fa7365",
+    "url": "/sengon_kavling.prototype/img/2.c677f401.jpg"
+  },
+  {
+    "revision": "f7166e9d7cef80c1c26b2dc4304eb284",
+    "url": "/sengon_kavling.prototype/img/2.f7166e9d.png"
+  },
+  {
+    "revision": "4c97593aae843b3638b6476fd5bc477b",
+    "url": "/sengon_kavling.prototype/img/3.4c97593a.jpg"
+  },
+  {
+    "revision": "771c815a26b6449e6d1d7e1bd4dc27df",
+    "url": "/sengon_kavling.prototype/img/3.771c815a.jpg"
+  },
+  {
+    "revision": "b20d09fe33ca50419ce3c2f884e89cec",
+    "url": "/sengon_kavling.prototype/img/3.b20d09fe.jpg"
+  },
+  {
+    "revision": "c04c5729f24f5af887ae31317c6e42e5",
+    "url": "/sengon_kavling.prototype/img/3.c04c5729.jpg"
+  },
+  {
+    "revision": "c677f40164ceb23bf44631fad9fa7365",
+    "url": "/sengon_kavling.prototype/img/3.c677f401.jpg"
+  },
+  {
+    "revision": "5e637d99f7bda3a99e5217a55e3a1678",
+    "url": "/sengon_kavling.prototype/img/4.5e637d99.jpg"
+  },
+  {
+    "revision": "ccab66127205c8e544c7ad556007e8cd",
+    "url": "/sengon_kavling.prototype/img/4.ccab6612.jpg"
+  },
+  {
+    "revision": "e03281ffaa34ccf91600dc8c5585c780",
+    "url": "/sengon_kavling.prototype/img/4.e03281ff.jpg"
+  },
+  {
+    "revision": "fc8313221da4649afc774a3068f4f27a",
+    "url": "/sengon_kavling.prototype/img/4c10e90c706c09d8bffb8955b9cbc832.png"
+  },
+  {
+    "revision": "9abf744ad8c62d06c77ec2a8bbbecf7a",
+    "url": "/sengon_kavling.prototype/img/4c5ca55b704576c8efe540f443d18f62.jpg"
+  },
+  {
+    "revision": "9cf0780f1944b38dff43019d766a741e",
+    "url": "/sengon_kavling.prototype/img/5.9cf0780f.jpg"
+  },
+  {
+    "revision": "a3948166855eabbd2cd6fbb1fa63f4e1",
+    "url": "/sengon_kavling.prototype/img/5.a3948166.jpg"
+  },
+  {
+    "revision": "56ab8cd6dd1ed6a10bdfd131627870b0",
+    "url": "/sengon_kavling.prototype/img/6.56ab8cd6.jpg"
+  },
+  {
+    "revision": "db7bdfbf9e09cbe21fad3e7d91d811c8",
+    "url": "/sengon_kavling.prototype/img/6.db7bdfbf.jpg"
+  },
+  {
+    "revision": "fc8313221da4649afc774a3068f4f27a",
+    "url": "/sengon_kavling.prototype/img/7.fc831322.jpg"
+  },
+  {
+    "revision": "4c97593aae843b3638b6476fd5bc477b",
+    "url": "/sengon_kavling.prototype/img/75dd406ebf16ede97710356db55af83e.jpg"
+  },
+  {
+    "revision": "06aef0648e995031ddcf52848daac61b",
+    "url": "/sengon_kavling.prototype/img/8.06aef064.jpg"
+  },
+  {
+    "revision": "3a5f0fe5a52a9c78d9fb7d584a4a66ab",
+    "url": "/sengon_kavling.prototype/img/9e6fb72e1fd586b94835e934edb4446a.jpg"
+  },
+  {
+    "revision": "ed0fb8bafa9cc229bf1137c08bd13743",
+    "url": "/sengon_kavling.prototype/img/be653ce9f15693e6f82110dc2de4493d.ed0fb8ba.jpg"
+  },
+  {
+    "revision": "e03281ffaa34ccf91600dc8c5585c780",
+    "url": "/sengon_kavling.prototype/img/c25b87f1c94bad782e41b13791781583.jpg"
+  },
+  {
+    "revision": "a3948166855eabbd2cd6fbb1fa63f4e1",
+    "url": "/sengon_kavling.prototype/img/cfa24060dea6fab80e91f3e45b73b270.jpg"
+  },
+  {
+    "revision": "06aef0648e995031ddcf52848daac61b",
+    "url": "/sengon_kavling.prototype/img/d395bd8c2d3e76605303c7c7ac5b8ffa.jpg"
+  },
+  {
+    "revision": "482c657bb3f2b5df0f83f18c5a2e91d0",
+    "url": "/sengon_kavling.prototype/img/dev.482c657b.png"
+  },
+  {
+    "revision": "3592bb05dc182118a7acb66d668d6f43",
+    "url": "/sengon_kavling.prototype/img/faq-illustration.3592bb05.png"
+  },
+  {
+    "revision": "9a7ee5c636b4f32e5a0ea15b3c4b68ea",
+    "url": "/sengon_kavling.prototype/img/legalitas.jpg"
+  },
+  {
+    "revision": "36b0579d37cdc8105f50a131bd193b59",
+    "url": "/sengon_kavling.prototype/img/logo1.36b0579d.png"
+  },
+  {
+    "revision": "36b0579d37cdc8105f50a131bd193b59",
+    "url": "/sengon_kavling.prototype/img/logo1.png"
+  },
+  {
+    "revision": "7a9e16dd2df85d06c190248a385707ed",
+    "url": "/sengon_kavling.prototype/img/logo2.7a9e16dd.png"
+  },
+  {
+    "revision": "7a9e16dd2df85d06c190248a385707ed",
+    "url": "/sengon_kavling.prototype/img/logo2.png"
+  },
+  {
+    "revision": "c372b1e0b4b0ce0d84c3e10a5f8aa37d",
+    "url": "/sengon_kavling.prototype/img/logo3.c372b1e0.jpg"
+  },
+  {
+    "revision": "56ab8cd6dd1ed6a10bdfd131627870b0",
+    "url": "/sengon_kavling.prototype/img/lokasi.jpg"
+  },
+  {
+    "revision": "2df0a6a3d0c3a6cb57c4783c15ed4992",
+    "url": "/sengon_kavling.prototype/img/pengukuran lahan.jpg"
+  },
+  {
+    "revision": "e9f5251251a96454de04e3f1f1648163",
+    "url": "/sengon_kavling.prototype/index.html"
+  },
+  {
+    "revision": "4f13a1773dcf83fdb123",
+    "url": "/sengon_kavling.prototype/js/about.8c273f7b.js"
+  },
+  {
+    "revision": "2f71a24c1ca20fb465f5",
+    "url": "/sengon_kavling.prototype/js/app.63830aff.js"
+  },
+  {
+    "revision": "0727bd517338c36be52c",
+    "url": "/sengon_kavling.prototype/js/chunk-vendors.fd8c40b3.js"
+  },
+  {
+    "revision": "77763ac66da2fc4b77b39c0a3982d6a2",
+    "url": "/sengon_kavling.prototype/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sengon_kavling.prototype/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sengon_kavling.prototype/precache-manifest.b0b5f9ea55450f53944e0e16d446a46f.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "grandviewsengon"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
